dbddaa13cbb0e3614124e2af26f7cc60324ae4e0
Louis-Kenzo Furuya Cahier <louiskenzo@gmail.com>
2012-11-10 14:29:20 +0900

    Added transitive search from GTK+ to Cairo.

--- FindGTK+.cmake
+++ FindGTK+.cmake
@@ -1,8 +1,7 @@
 ################################################################################
 #                                                                              #
-#                               Kyoto University                               #
-#                               Okuno laboratory                               #
-#                                Kappa project                                 #
+#                                  CodeCrafts                                  #
+#                                    Nuage                                     #
 #                                                                              #
 ################################################################################
 
@@ -21,15 +20,17 @@ set(GTK+_LIBRARY_FILE    gtk-${GTK+_FIND_VERSION_MAJOR}.${GTK+_FIND_VERSION_MINO
 
 # Determine package name and dependent versions based on version
 if (${GTK+_FIND_VERSION_MAJOR}.${GTK+_FIND_VERSION_MINOR} EQUAL 3.0)
-	set(    Pango_VERSION 1.0)
-	set(GDKPixbuf_VERSION 2.0)
-	set(      ATK_VERSION 1.0)
-	set(GTK+_LIBRARY_FILE gtk-${GTK+_FIND_VERSION_MAJOR})
+  set(    Pango_VERSION 1.0)
+  set(GDKPixbuf_VERSION 2.0)
+  set(      ATK_VERSION 1.0)
+  set(    CAIRO_VERSION 2.0)
+  set(GTK+_LIBRARY_FILE gtk-${GTK+_FIND_VERSION_MAJOR})
 elseif (${GTK+_FIND_VERSION_MAJOR}.${GTK+_FIND_VERSION_MINOR} EQUAL 2.0)
-	set(    Pango_VERSION 1.0)
-	set(GDKPixbuf_VERSION 2.0)
-	set(      ATK_VERSION 1.0)
-	set(GTK+_LIBRARY_FILE gtk-x11-${GTK+_FIND_VERSION_MAJOR}.${GTK+_FIND_VERSION_MINOR})
+  set(    Pango_VERSION 1.0)
+  set(GDKPixbuf_VERSION 2.0)
+  set(      ATK_VERSION 1.0)
+  set(    CAIRO_VERSION 2.0)
+  set(GTK+_LIBRARY_FILE gtk-x11-${GTK+_FIND_VERSION_MAJOR}.${GTK+_FIND_VERSION_MINOR})
 endif ()
 
 # Override parameters specific to platforms
@@ -42,20 +43,21 @@ endif ()
 # Search for dependencies
 set(FIND_PACKAGE_ARGUMENTS)
 if(GTK+_FIND_QUIETLY)
-	set(LFIND_PACKAGE_ARGUMENTS FIND_PACKAGE_ARGUMENTS QUIET)
+  set(LFIND_PACKAGE_ARGUMENTS FIND_PACKAGE_ARGUMENTS QUIET)
 endif()
 if(GTK+_FIND_REQUIRED)
-	set(FIND_PACKAGE_ARGUMENTS FIND_PACKAGE_ARGUMENTS REQUIRED)
+  set(FIND_PACKAGE_ARGUMENTS FIND_PACKAGE_ARGUMENTS REQUIRED)
 endif()
 find_package(Pango         ${Pango_VERSION} ${FIND_PACKAGE_ARGUMENTS})
 find_package(GDKPixbuf ${GDKPixbuf_VERSION} ${FIND_PACKAGE_ARGUMENTS})
 find_package(ATK             ${ATK_VERSION} ${FIND_PACKAGE_ARGUMENTS})
+find_package(Cairo         ${CAIRO_VERSION} ${FIND_PACKAGE_ARGUMENTS})
 
 # Under Unix systems -including Linux, OSX, and Cygwin- try using pkg-config to find GTK+
 find_package(PkgConfig)
 if (PKG_CONFIG_FOUND)
-	pkg_check_modules(PKGCONFIG_GTK+ QUIET ${GTK+_PKG_CONFIG_NAME})
-	set(GTK+_DEFINITIONS ${PKGCONFIG_GTK+_CFLAGS_OTHER})
+  pkg_check_modules(PKGCONFIG_GTK+ QUIET ${GTK+_PKG_CONFIG_NAME})
+  set(GTK+_DEFINITIONS ${PKGCONFIG_GTK+_CFLAGS_OTHER})
 endif (PKG_CONFIG_FOUND)
 
 # Extract the needed variables
@@ -94,8 +96,8 @@ find_library(GTK+_LIBRARY
                    /opt/local/lib)
 
 # Set the plural forms of the variables as is the convention
-set(GTK+_LIBRARIES    ${GTK+_LIBRARY};${PANGO_LIBRARIES};${GDKPIXBUF_LIBRARIES};${ATK_LIBRARIES})
-set(GTK+_INCLUDE_DIRS ${GTK+_INCLUDE_DIR};${GDK_CONFIG_INCLUDE_DIR};${PANGO_INCLUDE_DIRS};${GDKPIXBUF_INCLUDE_DIRS};${ATK_INCLUDE_DIRS})
+set(GTK+_LIBRARIES    ${GTK+_LIBRARY};${PANGO_LIBRARIES};${GDKPIXBUF_LIBRARIES};${ATK_LIBRARIES};${CAIRO_LIBRARIES})
+set(GTK+_INCLUDE_DIRS ${GTK+_INCLUDE_DIR};${GDK_CONFIG_INCLUDE_DIR};${PANGO_INCLUDE_DIRS};${GDKPIXBUF_INCLUDE_DIRS};${ATK_INCLUDE_DIRS};${CAIRO_INCLUDE_DIRS})
 
 include(FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set GTK+_FOUND to TRUE if all listed variables are TRUE
